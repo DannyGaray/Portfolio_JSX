d64a88e5e7a3d5db0eccb89c9afdb2ca21cdb2a5
Danny Garay <danielfrangaray@gmail.com>
2024-01-10 15:16:25 -0800

    src

---
+++ src/assets/index.js
@@ -0,0 +1,72 @@
+import logo from "./DanielLogo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+// ----------------------TECHNOLOGY SKILLS----------------------------
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import clang from "./tech/C_lang.png";
+import python from "./tech/Python.png";
+//------------------------ COMPANIES WORKED WITH --------------------------
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import HomeDepot from "./company/HomeDepot.png";
+import Uline from "./company/Uline.png";
+import Edro from "./company/Edro.png";
+//------------------------ Projects thumbnail --------------------------
+import snake from "./snake.png";
+import com from "./Command.png";
+import Portfoilo from "./Portfolio.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  clang,
+  python,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  HomeDepot,
+  Uline,
+  Edro,
+  snake,
+  com,
+  Portfoilo,
+};
